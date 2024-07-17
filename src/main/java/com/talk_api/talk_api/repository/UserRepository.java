b5fef10801f0e93d4fa60c334a333db722d18540
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-17 21:40:35 +0200

    build: user repository

---
+++ src/main/java/com/talk_api/talk_api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.talk_api.talk_api.repository;
+
+import com.talk_api.talk_api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
