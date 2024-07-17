cf90ae699aa4f4ee6da5a9120f34c26a36b8e757
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-17 20:26:14 +0200

    build: init repo

---
+++ src/test/java/com/talk_api/talk_api/TalkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talk_api.talk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
