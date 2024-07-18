bdc4a1308dfaa6019e3cc41d892926e4ffa167a9
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-18 20:34:13 +0200

    refactor: build command line to start server

--- src/main/java/com/talk_api/talk_api/TalkApiApplication.java
+++ src/main/java/com/talk_api/talk_api/TalkApiApplication.java
@@ -1,13 +1,24 @@
 package com.talk_api.talk_api;
 
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TalkApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TalkApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TalkApiApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(SocketIOServer server) {
+        return args -> {
+            server.start();
+            Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
+        };
+    }
 
 }
